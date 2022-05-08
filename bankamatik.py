adc2ebd81d8e567a2d8a41656228a9365b2f1a6a
MehmetFatih <100438870+MehmetFatihTaskin@users.noreply.github.com>
2022-05-08 22:42:34 +0300

    ilk kod ve hesapların tanımlanması

--- bankamatik.py
+++ bankamatik.py
@@ -1,5 +1,18 @@
-uwuwuwud
-asgfaksujf
-oldumu
-oldu 
-isim neden kırmızı oluyor
+import _tkinter as tk
+from tkinter import *
+
+fatihhesap = {
+    "isim" : "Fatih Taskin",
+    "hesapno": "12345678",
+    "bakiye": "5000",
+    "ekhesap": "2000"
+
+}
+
+tahahesap = {
+    "isim" : " Taha Bayrak",
+    "hesapno" : "87654321",
+    "bakiye" : "10000",
+    "ekhesap" : "2000"
+
+}
