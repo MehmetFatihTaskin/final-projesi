968168d48df40bfd932d9bf9095766a3dda4d037
MehmetFatih <100438870+MehmetFatihTaskin@users.noreply.github.com>
2022-05-08 23:08:17 +0300

    yazılar üstünde değişiklik

--- bankamatik.py
+++ bankamatik.py
@@ -38,7 +38,9 @@ def sign_in() :
                 fatihhesap["bakiye"]-=girilenTutar
                 yazi.config(text=f'hesapta {fatihhesap["bakiye"]} tl kaldı. ek hesapta {fatihhesap["ekHesap"]} tl kaldı.')
 
-print("oldumu")
+
+
+
 
 
 
